5d59c9e80957ad3a5cad7639841f45f26e90c964
dawei-wang <dawei-wang@users.noreply.github.com>
2023-01-25 13:31:06 -0800

    Full app without CSS

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Booking from "./Booking";
-// import Nav from "./Nav";
-// import Footer from "./Footer";
-// import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <Routes>
-      <Route>
-        {/* <Header />
-        <Nav /> */}
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/booking" element={<Booking />}></Route>
-        {/* <Footer /> */}
-      </Route>
-    </Routes>
+    <>
+      <Header />
+      <Nav />
+      <Routes>
+        <Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/booking" element={<Booking />}></Route>
+        </Route>
+      </Routes>
+      <Footer />
+    </>
   );
 };
 
