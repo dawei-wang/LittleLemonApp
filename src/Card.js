ecdfa3ae33d55fae2722b4de229fca05c769347f
dawei-wang <dawei-wang@users.noreply.github.com>
2023-01-29 12:48:25 -0800

    Add image imports

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,19 @@
 import React from "react";
+import seafood from "./Seafood.png";
+import brushetta from "./Brushetta.png";
+import stirFry from "./StirFry.png";
+
+const images = {
+  seafood,
+  brushetta,
+  stirFry,
+};
 
 const Card = ({ title, description, price, coverImg, location, openSpots }) => {
+  const image = images[coverImg];
   return (
     <div>
-      <img src={coverImg} alt={title} />
+      <img src={image} alt={title} />
       <h3>{title}</h3>
       <p>{description}</p>
       <p>Price: {price}</p>
